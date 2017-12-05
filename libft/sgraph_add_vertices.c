2c4b09b7fe31aa8d5a25040244e20a217793e6a1
tmwalo <tmwalo@student.wethinkcode.co.za>
2017-12-05 18:27:35 +0200

    add return zero on attempting to add duplicate vertex

--- libft/sgraph_add_vertices.c
+++ libft/sgraph_add_vertices.c
@@ -4,6 +4,7 @@ int	sgraph_add_vertices(t_llst *file, t_sgraph *pt_sgraph)
 {
 	t_llst	*current_node;
 	char	*line;
+	int	ret;
 
 	if ((file == NULL) || (pt_sgraph == NULL))
 		return (0);
@@ -12,7 +13,11 @@ int	sgraph_add_vertices(t_llst *file, t_sgraph *pt_sgraph)
 	{
 		line = (char *)current_node->value;
 		if (ft_isvertex(line))
-			sgraph_add_vertex(pt_sgraph, line);
+		{
+			ret = sgraph_add_vertex(pt_sgraph, line);
+			if (ret == 0)
+				return (0);
+		}
 		current_node = current_node->next;
 	}
 	return (1);
