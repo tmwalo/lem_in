3347ce09398da7ace3a9950bcba06d7a445470f5
tmwalo <tmwalo@student.wethinkcode.co.za>
2017-12-10 17:03:36 +0200

    update test to create only one list node

--- libft/tests/llst_delone_test.c
+++ libft/tests/llst_delone_test.c
@@ -6,25 +6,13 @@ int	main(void)
 	t_llst	*pt_lst;
 	t_llst	*pt_begin;
 	t_llst	*current_node;
-	t_llst	*last_node;
 
 	int	num1;
-	int	num2;
-	int	num3;
 
 	num1 = 187;
-	num2 = 777;
-	num3 = 139854;
 
 	pt_lst = llst_new(&num1, sizeof(int) );
 	pt_begin = pt_lst;
-	last_node = pt_lst;
-
-	pt_lst = llst_new(&num2, sizeof(int) );
-	llst_add(&pt_begin, pt_lst);
-
-	pt_lst = llst_new(&num3, sizeof(int) );
-	llst_add(&pt_begin, pt_lst);
 
 	current_node = pt_begin;
 	printf("Node values:");
@@ -35,7 +23,7 @@ int	main(void)
 	}
 	printf("\n");
 
-	llst_delone(&last_node);
+	llst_delone(&pt_begin);
 
 	current_node = pt_begin;
 	printf("Node values:");
@@ -45,6 +33,7 @@ int	main(void)
 		current_node = current_node->next;
 	}
 	printf("\n");
+	printf("%s\n", (pt_begin == NULL) ? "PASS" : "FAIL");
 
 	return (0);
 }
