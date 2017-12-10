26de66e7b23d416050429ec53ef6a211c257e537
tmwalo <tmwalo@student.wethinkcode.co.za>
2017-12-10 14:26:37 +0200

    add sgraph_print.c and call to graph_print

---
+++ libft/sgraph_print.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void	sgraph_print(t_sgraph *pt_sgraph)
+{
+	if (pt_sgraph == NULL)
+		return ;
+	ft_putendl("symbol graph:");
+	print_st(pt_sgraph->st_begin);
+	print_skeys(pt_sgraph);
+	graph_print(pt_sgraph->pt_graph);
+	ft_putstr("number of ants: ");
+	ft_putnbr(pt_sgraph->num_ants);
+	ft_putstr("\n");
+	ft_putstr("start room: ");
+	ft_putstr(pt_sgraph->start);
+	ft_putstr("\n");
+	ft_putstr("end room: ");
+	ft_putstr(pt_sgraph->end);
+	ft_putstr("\n");
+}
