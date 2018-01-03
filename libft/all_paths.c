2bae996abd45dab02ad4d7b4f8791beeaf3560d0
Terence MWALO <tmwalo@e4r14p7.wethinkcode.co.za>
2018-01-03 13:47:38 +0200

    fix: norm error

--- libft/all_paths.c
+++ libft/all_paths.c
@@ -6,7 +6,7 @@
 /*   By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/20 11:35:06 by tmwalo            #+#    #+#             */
-/*   Updated: 2017/12/20 11:35:22 by tmwalo           ###   ########.fr       */
+/*   Updated: 2018/01/03 13:44:15 by tmwalo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	all_paths(t_pathfinder *pt_map, int curr_vtx, int end)
 {
 	t_llst	*curr_node;
-	int		next_vertex;
 
 	if ((pt_map == NULL) || (curr_vtx < 0))
 		return ;
@@ -30,11 +29,10 @@ void	all_paths(t_pathfinder *pt_map, int curr_vtx, int end)
 		curr_node = (pt_map->pt_sgraph->pt_graph->adj_lsts)[curr_vtx];
 		while (curr_node != NULL)
 		{
-			next_vertex = *(int *)curr_node->value;
-			if (!((pt_map->visited)[next_vertex]))
+			if (!((pt_map->visited)[(*(int *)curr_node->value)]))
 			{
-				(pt_map->edge_to)[next_vertex] = curr_vtx;
-				all_paths(pt_map, next_vertex, end);
+				(pt_map->edge_to)[(*(int *)curr_node->value)] = curr_vtx;
+				all_paths(pt_map, (*(int *)curr_node->value), end);
 			}
 			curr_node = curr_node->next;
 		}
