7c0b21a30ff2572b6ea3e1d6930e638468a71b33
Terence MWALO <tmwalo@e4r14p7.wethinkcode.co.za>
2018-01-08 16:42:13 +0200

    add command equals start or end function

---
+++ libft/cmd_start_or_end.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_start_or_end.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/08 12:24:06 by tmwalo            #+#    #+#             */
+/*   Updated: 2018/01/08 12:38:53 by tmwalo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		cmd_start_or_end(char *cmd)
+{
+	int		is_start_or_end;
+
+	if ((cmd == NULL) || (ft_strlen(cmd) == 0))
+		return (0);
+	is_start_or_end = 0;
+	tokens = ft_strtoken(cmd);
+	if (ft_strcmp(*tokens, "##start") == 0)
+		is_start_or_end = 1;
+	else if (ft_strcmp(*tokens, "##end") == 0)
+		is_start_or_end = 1;
+	free_splitstr(&tokens);
+	if (is_start_or_end)
+		return (1);
+	else
+		return (0);
+}
