abc9242869b7e9d0e681ab2c6214ce0e7147d3bf
tmwalo <tmwalo@student.wethinkcode.co.za>
2018-01-12 09:32:36 +0200

    update test

--- libft/tests/path_delete_test.c
+++ libft/tests/path_delete_test.c
@@ -9,7 +9,6 @@ int	main(void)
 	t_pathfinder	*pt_map;
 	int				start;
 	int				end;
-	t_llst			*unviable_paths_ll;
 	t_llst		*current_node;
 	t_llst		*next_node;
 	t_llst		*current_node_path;
@@ -24,8 +23,7 @@ int	main(void)
 	start = *(int *)st_get(pt_map->pt_sgraph->st_begin, pt_map->pt_sgraph->start);
 	end = *(int *)st_get(pt_map->pt_sgraph->st_begin, pt_map->pt_sgraph->end);
 	all_paths(pt_map, start, end);
-	unviable_paths_ll = unviable_paths(pt_map->paths, start, end);
-	current_node = unviable_paths_ll;
+	current_node = pt_map->paths;
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
@@ -37,11 +35,24 @@ int	main(void)
 			current_node_path = current_node_path->next;
 		}
 		printf("\n");
-		path_delete(&(pt_map->paths), current_node_path);
 		current_node = next_node;
 	}
-
-	llst_del_nodes(&unviable_paths_ll);
+	printf("\nDelete 2nd path\n");
+	path_delete(&(pt_map->paths), (t_llst *)pt_map->paths->next->value);
+	current_node = pt_map->paths;
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		printf("path:");
+		current_node_path = (t_llst *)current_node->value;
+		while (current_node_path != NULL)
+		{
+			printf(" %s", (pt_map->pt_sgraph->keys)[*(int *)(current_node_path->value)]);
+			current_node_path = current_node_path->next;
+		}
+		printf("\n");
+		current_node = next_node;
+	}
 	pathfinder_destroy(&pt_map);
 	llst_del(&rev_file);
 	llst_del(&file);
