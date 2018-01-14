d73cbf01459dc5c0be45d649344e7f9572d49e03
tmwalo <tmwalo@student.wethinkcode.co.za>
2018-01-14 23:25:49 +0200

    improve memory deallocation on error

--- libft/st_node_new.c
+++ libft/st_node_new.c
@@ -15,21 +15,28 @@
 t_st_node	*st_node_new(void *key, int size_k, void *value, int size_v)
 {
 	t_st_node	*pt_st_node;
+	int		error;
 
 	if ((key == NULL) || (size_k <= 0) || (value == NULL) || (size_v <= 0))
 		return (NULL);
 	pt_st_node = (t_st_node *)malloc(sizeof(t_st_node));
 	if (pt_st_node == NULL)
 		return (NULL);
-	pt_st_node->key = malloc(size_k);
-	if (pt_st_node->key == NULL)
-		return (NULL);
+	error = 0;
+	if ((pt_st_node->key = malloc(size_k)) == NULL)
+		error = 1;
 	ft_memmove(pt_st_node->key, key, size_k);
 	pt_st_node->size_key = size_k;
-	pt_st_node->value = malloc(size_v);
-	if (pt_st_node->value == NULL)
-		return (NULL);
+	if ((pt_st_node->value = malloc(size_v)) == NULL)
+		error = 1;
 	ft_memmove(pt_st_node->value, value, size_v);
 	pt_st_node->size_value = size_v;
+	if (error)
+	{
+		ft_memdel((void **)(&(pt_st_node->key)));
+		ft_memdel((void **)(&(pt_st_node->value)));
+		ft_memdel((void **)(&(pt_st_node)));
+		return (NULL);
+	}
 	return (pt_st_node);
 }
